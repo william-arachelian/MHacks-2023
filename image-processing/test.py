1a459403c72d9ca417d23f8a43dfe036a1a9b595
Jason Shi <jasonshi08@gmail.com>
2023-11-18 16:42:41 -0500

    paddleocr

--- image-processing/test.py
+++ image-processing/test.py
@@ -1,16 +1,15 @@
+from paddleocr import PaddleOCR,draw_ocr
 import cv2
-import pytesseract
-from paddleocr import PaddleOCR
-# import pandas as pd
-
-#reader = easyocr.Reader(['en'], gpu = True) # this needs to run only once to load the model into memory
-img_cv = cv2.imread("IMG_7505.jpg")
-gray = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
+import numpy as np
+ocr = PaddleOCR(lang='en') # need to run only once to download and load model into memory
+img_path = 'IMG_7505.jpg'
+image = cv2.imread(img_path)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-ocr = PaddleOCR(use_angle_cls=True)
-result = ocr.ocr(img_cv)
+result = ocr.ocr(gray)
+
 for idx in range(len(result)):
     res = result[idx]
     for line in res:
-        print(line)
+        print(line[1][0])
