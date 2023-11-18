9d0aab71dfc99c5475bdb4ecbe1370b3c28fcb01
Jason Shi <jasonshi08@gmail.com>
2023-11-18 13:45:42 -0500

    add image

---
+++ image-processing/test.py
@@ -0,0 +1,12 @@
+import cv2
+import pytesseract
+
+img_cv = cv2.imread(r'/image-processing/thing.JPG')
+
+# By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
+# we need to convert from BGR to RGB format/mode:
+img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+print(pytesseract.image_to_string(img_rgb))
+# OR
+img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
+print(pytesseract.image_to_string(img_rgb))
